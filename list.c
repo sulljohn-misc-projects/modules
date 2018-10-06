c679c7f7747c7eefe17a5806311724f9c6311384
John Sullivan <sulljohn12@gmail.com>
2018-10-05 20:52:41 -0400

    Added some more new files.

--- list.c
+++ list.c
@@ -2,3 +2,30 @@
 // Created by John Sullivan on 10/5/18.
 //
 
+#include "list.h"
+
+/* put(): place a car at the beginning of the list
+ * returns 0 if successful; nonzero otherwise
+ */
+int32_t lput(car_t *cp) {
+
+}
+
+/* get(): remove and return the first car in the list;
+ * return NULL if the list is empty
+ */
+car_t *lget() {
+
+}
+
+/* apply a function to every car in the list */
+void lapply(void (*fn)(car_t *cp)) {
+
+}
+
+/* remove(): find, remove, and return any car with
+ * the designated plate; return NULL if not present
+ */
+car_t *lremove(char *platep) {
+
+}
