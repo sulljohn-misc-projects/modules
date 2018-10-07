d4e0ff1331b8d852dc3a72600c81effcfbd0695c
John Sullivan <sulljohn12@gmail.com>
2018-10-06 20:46:30 -0400

    Adjusted hash_test.c more

--- hash_test.c
+++ hash_test.c
@@ -8,53 +8,53 @@
 #include "list.h"
 
 
-car_t *make_car(char* plate, double price, int year) {
-	car_t* pp;      
+car_t *make_car(char *plate, double price, int year) {
+    car_t *pp;
     // Allocating memory, returning if fails
-    if (!(pp = (car_t*)malloc(sizeof(car_t)))) {                               
-			printf("[Error: malloc failed allocating car]\n");
-			return NULL;                                            
+    if (!(pp = (car_t *) malloc(sizeof(car_t)))) {
+        printf("[Error: malloc failed allocating car]\n");
+        return NULL;
     }
-	
-	// Setting parts of object based on criteria                            
-	pp->next = NULL;                          
-	pp->price = price;                            
-	pp->year = year;                   
-	strcpy(pp->plate, plate);
-	return pp;                                               
+
+    // Setting parts of object based on criteria
+    pp->next = NULL;
+    pp->price = price;
+    pp->year = year;
+    strcpy(pp->plate, plate);
+    return pp;
 }
 
 void print_plate(car_t *cp) {
-	printf("%s\n", cp->plate);
+    printf("%s\n", cp->plate);
 }
 
-bool searchfn(void* elementp,const void* keyp) {
-    car_t* carp = (car_t*)elementp;
+bool searchfn(void *elementp, const void *keyp) {
+    car_t *carp = (car_t *) elementp;
 
-    return strcmp(carp->plate,keyp)==0;
-} 
+    return strcmp(carp->plate, keyp) == 0;
+}
 
 int main() {
-	car_t *p1 = make_car("123456789",20000,2016);
-	car_t *p2 = make_car("098765432",15000,2015);
-	car_t *p3 = make_car("543216789",17000,2017);
-	car_t *p4 = make_car("678905432",12000,2012);
+    car_t *p1 = make_car("123456789", 20000, 2016);
+    car_t *p2 = make_car("098765432", 15000, 2015);
+    car_t *p3 = make_car("543216789", 17000, 2017);
+    car_t *p4 = make_car("678905432", 12000, 2012);
 
-	hashtable_t* hash = hopen(1024);
+    hashtable_t *hash = hopen(4);
 
-	hput(hash,p1,p1->plate,sizeof(p1->plate));
-	hput(hash,p2,p2->plate,sizeof(p2->plate));
-	hput(hash,p3,p3->plate,sizeof(p3->plate));
-	hput(hash,p4,p4->plate,sizeof(p4->plate));
+    hput(hash, p1, p1->plate, sizeof(p1->plate));
+    hput(hash, p2, p2->plate, sizeof(p2->plate));
+    hput(hash, p3, p3->plate, sizeof(p3->plate));
+    hput(hash, p4, p4->plate, sizeof(p4->plate));
 
-	printf("printing hash...\n");
-	happly(hash,(void (*)(void*))print_plate);
+    printf("printing hash...\n");
+    happly(hash, (void (*)(void *)) print_plate);
 
-	printf("Searching for hash with search function");
-	car_t *tmp = searchfn(hash, p1);
+    printf("Searching for hash with search function");
+    car_t *tmp = hsearch(hash, searchfn, "123456789", sizeof("123456789"));
     printf("%s\n", tmp->plate);
 
-	hclose(hash);
-	
-	return 0;
+    hclose(hash);
+
+    return 0;
 }
